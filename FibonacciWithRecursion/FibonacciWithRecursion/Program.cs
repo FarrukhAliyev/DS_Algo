3e0368db72144ca68bf764f5b9637d70c6d2d81f
Farrukh Aliyev <farrukh.aliyev@outlook.com>
2024-10-06 20:21:08 +0400

    Fibonacci

---
+++ FibonacciWithRecursion/FibonacciWithRecursion/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FibonacciWithRecursion;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Fibonacci(6));
+    }
+
+    public static int Fibonacci(int position)
+    {
+        if (position < 0) 
+            throw new ArgumentOutOfRangeException();
+
+        switch (position)
+        {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            default:
+                return Fibonacci(position - 2) + Fibonacci(position - 1);
+        };
+            
+    }
+}
